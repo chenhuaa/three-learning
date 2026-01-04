3b384a6418cbfa6bc963f869fdb35c83fd2a1ff6
chenhuaa <33261183+chenhuaa@users.noreply.github.com>
2026-01-04 17:09:55 +0800

    Update index.vue

--- src/components/test1/index.vue
+++ src/components/test1/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <div ref="resizableDiv" class="resizable" @mousedown="onMouseDown" :style="divStyle">
+  <div
+    ref="resizableDiv"
+    class="resizable"
+    @mousedown="onMouseDown"
+    :style="divStyle"
+  >
     <div class="resizer" @mousedown="onResizeMouseDown"></div>
   </div>
 </template>
