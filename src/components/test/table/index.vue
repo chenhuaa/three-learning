475668cee4065d1451df63a68bb257d4267e0c14
chen-hua369 <cha_chenhua@163.com>
2026-01-05 18:42:26 +0800

    feat: 测试

--- src/components/test/table/index.vue
+++ src/components/test/table/index.vue
@@ -2,19 +2,16 @@
   <table v-if="filteredData.length">
     <thead>
       <tr>
-        <th v-for="key in columns"
-          @click="sortBy(key)"
-          :class="{ active: sortKey == key }">
+        <th v-for="key in columns" @click="sortBy(key)" :class="{ active: sortKey == key }">
           {{ capitalize(key) }}
-          <span class="arrow" :class="sortOrders[key] > 0 ? 'asc' : 'dsc'">
-          </span>
+          <span class="arrow" :class="sortOrders[key] > 0 ? 'asc' : 'dsc'"></span>
         </th>
       </tr>
     </thead>
     <tbody>
       <tr v-for="entry in filteredData">
         <td v-for="key in columns">
-          {{entry[key]}}
+          {{ entry[key] }}
         </td>
       </tr>
     </tbody>
